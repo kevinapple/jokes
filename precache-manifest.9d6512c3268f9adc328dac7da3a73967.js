631029e313e28148bda084807c8a14bb5aa1e325
kevinwit <kevin.zou@honeywell.com>
2019-08-12 16:20:36 +0800

    Updates

---
+++ precache-manifest.9d6512c3268f9adc328dac7da3a73967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a62adf5ef5ad09bffb04f8ab6e84da",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "bb2b60bc2f4d4395e99e",
+    "url": "/jokes/static/css/main.dcdad4cb.chunk.css"
+  },
+  {
+    "revision": "bade343a62241c9bb584",
+    "url": "/jokes/static/js/2.d4f817a2.chunk.js"
+  },
+  {
+    "revision": "bb2b60bc2f4d4395e99e",
+    "url": "/jokes/static/js/main.6bb2abc1.chunk.js"
+  },
+  {
+    "revision": "4648a4e5b6036d4c20fd",
+    "url": "/jokes/static/js/runtime~main.289949f1.js"
+  }
+]);
